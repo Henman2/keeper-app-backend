92552c423edefd8d9a46a94462ea7bccedc7bf7a
Henman2 <47754370+Henman2@users.noreply.github.com>
2024-05-15 18:54:17 +0000

    Add Routes: notes, newnote, updatenote and deletenote

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const noteRouter = express.Router();
+const noteController = require('../controllers/noteController'); 
 
+/* GET home page. */
+noteRouter.get('/', noteController.showNotes );
+noteRouter.post('/newnote', noteController.createNote);
+noteRouter.delete('/deletenote', noteController.deleteNote);
+noteRouter.put('/updatenote', noteController.updateNote);
+
+module.exports = noteRouter;
