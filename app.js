b3bc16b6fc3642e5126804d06a37085433368863
Henman2 <47754370+Henman2@users.noreply.github.com>
2024-12-19 22:55:34 +0000

    Update cors to include new frontend url

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 const express = require('express');
 const cors = require('cors');
-// const indexRouter = require('./routes/index');
 const userRouter = require('./routes/userRoutes');
 const noteRouter = require('./routes/noteRoutes');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-//CORS Setup
-app.use((req, res, next) =>{
-  res.header("Access-Control-Allow-Origin", true);
-  res.header("Access-Control_Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers","X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-  next();
-})
-app.use(cors({
+// CORS Setup
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://keeper-app-frontend-eta.vercel.app',
+];
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000',
-    allowedHeaders:["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps, curl, etc.)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   })
-)
-app.set("trust proxy", 1); //properly get the client's IP address.
+);
+app.set('trust proxy', 1); // Properly get the client's IP address
 
-//set views
+// Set views
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static('./public'));
 
-//Express routes
-// app.use('/', indexRouter);
+// Express routes
 app.use('/users', userRouter);
 app.use('/notes', noteRouter);
 
