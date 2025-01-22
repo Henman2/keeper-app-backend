435df6b472d010c7fac02ca1364185bfc6813524
Henman2 <47754370+Henman2@users.noreply.github.com>
2025-01-22 19:02:53 +0000

    Update jwt token: set samesite to none

--- utiliti/generateJWT.js
+++ utiliti/generateJWT.js
@@ -7,7 +7,7 @@ const generateJWT = (res, userId) => {
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-        sameSite: 'strict',  // Prevent CSRF attacks
+        sameSite: 'none',  // Allow crosssite requests
         maxAge: 30*24*60*60*1000, //30days
     })
 }
